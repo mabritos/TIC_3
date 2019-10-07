b4aac2a7974e3b6951e0fea380311f19c18f4d6e
fbadano <ferbadano20@gmail.com>
2019-10-07 13:33:22 -0300

    Base de Datos Setup y Routers

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
 var express = require('express');
+const session = require('express-session');
+const userRouter = require('./routes/user');
+const characterRouter = require('./routes/character');
 var app = express();
 var serv = require('http').Server(app);
+
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/client/index.html');
 });
 app.use('/client', express.static(__dirname + '/client'));
 
-serv.listen(2000);
+app.use(session({
+    secret:'youtube_video',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 60 * 1000 * 30
+    }
+}));
+
+app.use('/user', userRouter);
+
+app.use('/character', characterRouter);
+
+
+app.use((req, res, next) =>  {
+    var err = new Error('Page not found');
+    err.status = 404;
+    next(err);
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.send(err.message);
+});
+
+serv.listen(2000, 'localhost');
+
+module.exports = app;
---
+++ routes/character.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const User = require('../core/user');
+const router = express.Router();
+
+// create an object from the class User in the file core/user.js
+const user = new User();
+
+router.use('/attack', (req, res) => {
+	//TODO
+	res.send('attack');
+});
+
+module.exports = router;
