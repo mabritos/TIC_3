db4d1ea2c13acfec3c29b104be14e09666bb5cad
fbadano <ferbadano20@gmail.com>
2019-11-08 12:32:37 -0300

    printXp funcionando

--- client/js/game.js
+++ client/js/game.js
@@ -1,13 +1,9 @@
-
-
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 
 async function printFight(gameLog, character, enemy, loot) {
-    $("#gameImage").attr("src", "/client/img/enemies/"+enemy.icon);
-
     for (let index = 0; index < gameLog.length; index++) {
         const element = gameLog[index];
         $("#gameLog").html($("#gameLog").html() + element + "<br>");
@@ -16,6 +12,8 @@ async function printFight(gameLog, character, enemy, loot) {
     }
     if (character)
         printCharacter(character);
+    if (enemy)
+        $("#gameImage").attr("src", "/client/img/enemies/"+enemy.icon);
     if (loot)
         printLoot(loot);
 }
@@ -34,59 +32,51 @@ function equipItem(character){
     });
 }
 
+var xpLevels = [0, 100, 250, 500, 1000, 2500];
+
 function printXp(xp) {
     let xpTop = getXpTop(xp);
-    current_progress = xp/xpTop*100;
-    $(".progress-bar").css("width", current_progress + "%").attr("aria-valuenow", current_progress)
-    $(".progress").notify(xp + '/' + xpTop, {
+    current_progress = (xp - xpLevels[xpTop - 1]) / xpLevels[xpTop] * 100;
+    $(".progress-bar").css("width", current_progress + "%").attr("aria-valuenow", current_progress);
+    $(".progress").notify(xp + '/' + xpLevels[xpTop], {
       style: 'rpg', className: "xp", position: "right"
     });
+    $("#lvl").html(xpTop);
 }
 
 function getXpTop(xp) {
-    let xpLevels = [100, 250, 500, 1000, 2500]
-    xpLevels.forEach(xpTop => {
-        console.log(xp < xpTop)
-        if (xp < xpTop) {
-            return xpTop;
-        }
-    });
+    for (i = 0; i < xpLevels.length; i++)
+        if (xp < xpLevels[i])
+            return i;
+    return 0;
 }
 
 function printCharacter(character, items) {
     $("#characterName").html(character.name);
-    $("#characterXp")// TO DO
     $("#characterGold").html(character.gold).fadeIn();
     $("#characterHp").html(character.hp);
     $("#characterStrength").html(character.strength);
     $("#characterAgility").html(character.agility);
     $("#characterIntelligence").html(character.intelligence);
+    printXp(character.xp);
     if(items) {
         $("#characterArmor").attr("src", "/client/img/items/" + items.armor.icon);
         $("#characterWeapon").attr("src", "/client/img/items/" + items.weapon.icon);
         $("#characterArmorName").html(items.armor.name);
         $("#characterWeaponName").html(items.weapon.name);
     }
-    /*
-    $.post("/character/getItem", function(items) {
-        $("#characterWeaponName").html(items.Armor.name);
-        $("#characterArmorName").html(items.Weapon.name);
-        $("#characterArmor").attr("src","/client/img/items/" + items.Armor.icon); //no estoy seguro que se pueda escribir la url de la img asi
-        $("#characterWeapon").attr("src","/client/img/items/" + items.Weapon.icon);
-    });
-     */
 }
 
 function characterLoad(character = null) {
     if (character === null)
         $.post("/character/load", function (character) {
-            $.post('/character/getItems',{"character": character} , function(items){
-                printCharacter(character,items);
+            $.post('/character/getItems', {"character": character}, function(items){
+                printCharacter(character, items);
             });
         });
     else {
-        $.post('/character/getItems',{"character": character} , function(items){
-            printCharacter(character,items);
+        $.post('/character/getItems', {"character": character}, function(items){
+            printCharacter(character, items);
         });
     }
 }
@@ -102,6 +92,5 @@ function fight() {
 }
 
 $(function () {
-
     characterLoad();
 });
