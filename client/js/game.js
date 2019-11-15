ca5a81652ae6dd2a761fa6c9f6efe426f4fafdf4
mabritos <mabritos@correo.um.edu.uy>
2019-11-15 10:57:51 -0300

    LootSelector arreglado

--- client/js/game.js
+++ client/js/game.js
@@ -40,7 +40,8 @@ function equipItem(){
                 printCharacter(data.character, {"weapon": data.item});
         });
     $("#loot").addClass('d-none');
-    $("#loot").empty();
+    $("#lootSelector").empty();
+
 }
 
 var xpLevels = [0, 100, 250, 500, 1000, 2500];
